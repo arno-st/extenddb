0fcd06a509df974e76ac6ad4467714dabf8d1ad1
Arno Streuli <arno@daika08.ch>
2018-11-02 14:17:40 +0100

    Add files via upload

--- setup.php
+++ setup.php
@@ -66,11 +66,11 @@ function extenddb_check_upgrade() {
 			WHERE directory='" . $version['name'] . "' ");
 
 		if( $old < '1.1' ) {
-			api_plugin_db_add_column ('extenddb', 'host', array('name' => 'serial_no', 'type' => 'char(20)', 'NULL' => false, 'default' => ''));
-			api_plugin_db_add_column ('extenddb', 'host', array('name' => 'type', 'type' => 'char(50)', 'NULL' => false, 'default' => ''));
+			api_plugin_db_add_column ('extenddb', 'host', array('name' => 'serial_no', 'type' => 'char(20)', 'NULL' => true, 'default' => ''));
+			api_plugin_db_add_column ('extenddb', 'host', array('name' => 'type', 'type' => 'char(50)', 'NULL' => true, 'default' => ''));
 		}
 		if( $old < '1.1.2' ) {
-			api_plugin_db_add_column ('extenddb', 'host', array('name' => 'isPhone', 'type' => 'char(2)', 'NULL' => false, 'default' => ''));
+			api_plugin_db_add_column ('extenddb', 'host', array('name' => 'isPhone', 'type' => 'char(2)', 'NULL' => true, 'default' => ''));
 		}
 
 	}
