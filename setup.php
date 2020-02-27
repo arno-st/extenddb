6bc872519b7f859e37afe51fd92945e8d4c97e66
Arno Streuli <arno@daika08.ch>
2020-02-27 10:45:40 +0100

    Add files via upload
    1.2.2 many upgrafde to be able to retrive SysObjId and based on that serial number
    Only Cisco 4500 and 9500 are pooled for VSS (so 2 nodes), but I will have to improve it to detect if we have 2 nodes or not

--- setup.php
+++ setup.php
@@ -26,6 +26,8 @@ function plugin_extenddb_install () {
 	api_plugin_register_hook('extenddb', 'config_settings', 'extenddb_config_settings', 'setup.php');
 	api_plugin_register_hook('extenddb', 'config_form', 'extenddb_config_form', 'setup.php');
 	api_plugin_register_hook('extenddb', 'api_device_new', 'extenddb_api_device_new', 'setup.php');
+	api_plugin_register_hook('extenddb', 'utilities_action', 'extenddb_utilities_action', 'setup.php');
+	api_plugin_register_hook('extenddb', 'utilities_list', 'extenddb_utilities_list', 'setup.php');
 
 }
 
@@ -33,11 +35,10 @@ function plugin_extenddb_uninstall () {
 	// Do any extra Uninstall stuff here
 
 	// Remove items from the settings table
-	db_execute('ALTER TABLE host DROP COLUMN serial_no, DROP COLUMN type, DROP COLUMN isPhone');
+	db_execute('ALTER TABLE host DROP COLUMN serial_no, DROP COLUMN type, DROP COLUMN isPhone, DROP COLUMN SysObjId');
 }
 
 function plugin_extenddb_check_config () {
-	global $config;
 	// Here we will check to ensure everything is configured
 	extenddb_check_upgrade ();
 
@@ -51,6 +52,8 @@ function plugin_extenddb_upgrade () {
 }
 
 function extenddb_check_upgrade() {
+	global $config;
+
 	$version = plugin_extenddb_version ();
 	$current = $version['version'];
 	$old     = read_config_option('plugin_extenddb_version');
@@ -71,6 +74,9 @@ function extenddb_check_upgrade() {
 		}
 		if( $old < '1.1.2' ) {
 			api_plugin_db_add_column ('extenddb', 'host', array('name' => 'isPhone', 'type' => 'char(2)', 'NULL' => true, 'default' => ''));
+		}
+	if( $old < '1.2.1' ) {
+			api_plugin_db_add_column ('extenddb', 'host', array('name' => 'SysObjId', 'type' => 'char(50)', 'NULL' => true, 'default' => ''));
 		}
 
 	}
@@ -112,45 +118,82 @@ function extenddb_config_form () {
 				'value' => '|arg1:isPhone|',
 				'default' => '',
 			);
+			$fields_host_edit3['SysObjId'] = array(
+				'friendly_name' => 'SysObjId',
+				'description' => 'Sys Object ID definition',
+				'method' => 'textbox',
+				'max_length' => 50,
+				'value' => '|arg1:SysObjId|',
+				'default' => '',
+			);
 		}
 	}
 	$fields_host_edit = $fields_host_edit3;
 }
 
-function extenddb_api_device_new ($save) {
-	$snmptype		 = ".1.0.8802.1.1.2.1.5.4795.1.2.7.0";
-	$snmpserialno		= ".1.3.6.1.2.1.47.1.1.1.1.11.1001";
+function extenddb_utilities_list () {
+	global $colors;
+	html_header(array("extenddb Plugin"), 4);
+	form_alternate_row();
+	?>
+		<td class="textArea">
+			<a href='utilities.php?action=extenddb_rebuild'>Complete Serial Number and SysObjId.</a>
+		</td>
+		<td class="textArea">
+			Complete Serial Number anb SysObjId of all non filed device
+		</td>
+	<?php
+	form_end_row();
+}
 
-	if( isset($_POST['serial_no']) ){
-		if ( empty($_POST['serial_no']) ) {
-			$save['serial_no'] = parseDevice($save, $snmpserialno );
-		} else {
-			$save['serial_no'] = form_input_validate($_POST['serial_no'], 'serial_no', '', true, 3);
+function extenddb_utilities_action ($action) {
+	// get device list,  where serial number is empty, or sysobjID
+	$dbquery = db_fetch_assoc("SELECT  * FROM host WHERE serial_no is NULL OR SysObjId IS NULL OR serial_no = '' OR SysObjId = '' ORDER BY id");
+	if ( ($dbquery > 0) && $action == 'extenddb_rebuild' ){
+		if ($action == 'extenddb_rebuild') {
+		// Upgrade the map address table
+			foreach ($dbquery as $host) {
+				extenddb_api_device_new( $host );
+			}
 		}
-	} else {
-		$save['serial_no'] = parseDevice($save, $snmpserialno );
-	}
-	
-	if( isset($_POST['type']) ){
-		if ( empty($_POST['type']) ) {
-			$save['type'] = parseDevice($save, $snmptype );
-		} else {
-			$save['type'] = form_input_validate($_POST['type'], 'type', '', true, 3);
-		}
-	} else {
-		$save['type'] = parseDevice($save, $snmptype );
+		include_once('./include/top_header.php');
+		utilities();
+		include_once('./include/bottom_footer.php');
+	} 
+	return $action;
+}
+
+function extenddb_api_device_new ($hostrecord_array) {
+
+	$snmpsysobjid		 = ".1.3.6.1.2.1.1.2.0"; // return Cisco OID type
+
+	// don't do it for disabled
+	if( $hostrecord_array['disabled'] == 'on' ) {
+		return $hostrecord_array;
 	}
 
-	if( isset($_POST['isPhone']) ){
-		if ( empty($_POST['isPhone']) ) {
-			$save['isPhone'] = false;
-		} else {
-			$save['isPhone'] = form_input_validate($_POST['isPhone'], 'isPhone', '', true, 3);
-		}
-	} else {
-		$save['isPhone'] = false;
+	// look for the equipement type
+	$searchtype = cacti_snmp_get( $hostrecord_array['hostname'], $hostrecord_array['snmp_community'], $snmpsysobjid,
+	$hostrecord_array['snmp_version'], $hostrecord_array['snmp_username'], $hostrecord_array['snmp_password'], 
+	$hostrecord_array['snmp_auth_protocol'], $hostrecord_array['snmp_priv_passphrase'], 
+	$hostrecord_array['snmp_priv_protocol'], $hostrecord_array['snmp_context'] ); 
+
+	if( strcmp( $searchtype, 'U' ) == 0 ) {
+extdb_log("recu: ". $hostrecord_array['description'] );
+extdb_log("SysObjId: ". $searchtype );
+		return $hostrecord_array;
 	}
-	return $save;
+
+	// find and store device SysObjId
+	$hostrecord_array['SysObjId'] = trim( substr($searchtype, strpos( $searchtype, ':' )+1) );
+	db_execute("update host set SysObjId='".$hostrecord_array['SysObjId']. "' where id=" . $hostrecord_array['id'] );
+
+
+	// find and store Serial Number 
+	$hostrecord_array['serial_no'] = getSN( $hostrecord_array, $hostrecord_array['SysObjId'] );
+	db_execute("update host set serial_no='".$hostrecord_array['serial_no']. "' where id=" . $hostrecord_array['id'] );
+
+	return $hostrecord_array;
 }
 
 function extenddb_config_settings () {
@@ -158,21 +201,6 @@ function extenddb_config_settings () {
 
 	if (isset($_SERVER['PHP_SELF']) && basename($_SERVER['PHP_SELF']) != 'settings.php')
 		return;
-/*
-	$tabs['misc'] = 'Misc';
-
-	$temp = array(
-		'extenddb' => array(
-			'friendly_name' => 'ExtendDB',
-			'method' => 'spacer',
-			),
-	);
-
-	if (isset($settings['misc']))
-		$settings['misc'] = array_merge($settings['misc'], $temp);
-	else
-		$settings['misc']=$temp;
-*/
 }
 
 function extenddb_check_dependencies() {
@@ -181,13 +209,85 @@ function extenddb_check_dependencies() {
 	return true;
 }
 
-function parseDevice( $hostrecord_array, $OID ) {
+function getSN( $hostrecord_array, $SysObjId ){
+	$SysObjId = substr($SysObjId, strrpos( $SysObjId, '.' )+1 );
 
-	$ret = cacti_snmp_get( $hostrecord_array['hostname'], $hostrecord_array['snmp_community'], $OID, $hostrecord_array['snmp_version'], $hostrecord_array['snmp_username'], $hostrecord_array['snmp_password'], $hostrecord_array['snmp_auth_protocol'], $hostrecord_array['snmp_priv_passphrase'], $hostrecord_array['snmp_priv_protocol'], $hostrecord_array['snmp_context'] );
+	switch( $SysObjId ) {
+	case "1732": // WS-C4500X-32
+		$snmpserialno = ".1.3.6.1.2.1.47.1.1.1.1.11.500"; // module 1
+
+		$serialno = cacti_snmp_get( $hostrecord_array['hostname'], $hostrecord_array['snmp_community'], $snmpserialno, 
+		$hostrecord_array['snmp_version'], $hostrecord_array['snmp_username'], $hostrecord_array['snmp_password'], 
+		$hostrecord_array['snmp_auth_protocol'], $hostrecord_array['snmp_priv_passphrase'], $hostrecord_array['snmp_priv_protocol'],
+		$hostrecord_array['snmp_context'] );
+
+		$snmpserialno = ".1.3.6.1.2.1.47.1.1.1.1.11.1000"; // module 2
+
+		$serialno = $serialno . " ". cacti_snmp_get( $hostrecord_array['hostname'], $hostrecord_array['snmp_community'], $snmpserialno,
+		$hostrecord_array['snmp_version'], $hostrecord_array['snmp_username'], $hostrecord_array['snmp_password'], 
+		$hostrecord_array['snmp_auth_protocol'], $hostrecord_array['snmp_priv_passphrase'], $hostrecord_array['snmp_priv_protocol'], 
+		$hostrecord_array['snmp_context'] );
+		break;
+
+	case "2593": // C9500-16X
+		$snmpserialno = ".1.3.6.1.2.1.47.1.1.1.1.11.1000"; // module 1
+
+		$serialno = cacti_snmp_get( $hostrecord_array['hostname'], $hostrecord_array['snmp_community'], $snmpserialno, 
+		$hostrecord_array['snmp_version'], $hostrecord_array['snmp_username'], $hostrecord_array['snmp_password'], 
+		$hostrecord_array['snmp_auth_protocol'], $hostrecord_array['snmp_priv_passphrase'], $hostrecord_array['snmp_priv_protocol'],
+		$hostrecord_array['snmp_context'] );
+
+		$snmpserialno = ".1.3.6.1.2.1.47.1.1.1.1.11.2000"; // module 2
+
+		$serialno = $serialno . " ". cacti_snmp_get( $hostrecord_array['hostname'], $hostrecord_array['snmp_community'], $snmpserialno,
+		$hostrecord_array['snmp_version'], $hostrecord_array['snmp_username'], $hostrecord_array['snmp_password'], 
+		$hostrecord_array['snmp_auth_protocol'], $hostrecord_array['snmp_priv_passphrase'], $hostrecord_array['snmp_priv_protocol'], 
+		$hostrecord_array['snmp_context'] );
+		break;
+
+	case "1410": // Nexus 5672UP
+	case "1084": // Nexus 5548UP
+		$snmpserialno = ".1.3.6.1.2.1.47.1.1.1.1.11.22";
+
+		$serialno = cacti_snmp_get( $hostrecord_array['hostname'], $hostrecord_array['snmp_community'], $snmpserialno, 
+		$hostrecord_array['snmp_version'], $hostrecord_array['snmp_username'], $hostrecord_array['snmp_password'], 
+		$hostrecord_array['snmp_auth_protocol'], $hostrecord_array['snmp_priv_passphrase'], $hostrecord_array['snmp_priv_protocol'], 
+		$hostrecord_array['snmp_context'] );
+		break;
 
 
-	return $ret;
+        case "324": // WS-C2950
+        case "359": // WS-C2950
+        case "540": // WS-C2940
+	case "578": // 2800 
+	case "837": // CISCO881
+	case "857": // CISCO891
+	case "1378": // C819G-G-U
+	case "1858": // C891F
+	case "1041": // 3945
+	case "2059": // C819G-4G-GA-K9
+                $snmpserialno = ".1.3.6.1.2.1.47.1.1.1.1.11.1";
 
+                $serialno = cacti_snmp_get( $hostrecord_array['hostname'], $hostrecord_array['snmp_community'], $snmpserialno,
+                $hostrecord_array['snmp_version'], $hostrecord_array['snmp_username'], $hostrecord_array['snmp_password'],
+                $hostrecord_array['snmp_auth_protocol'], $hostrecord_array['snmp_priv_passphrase'], $hostrecord_array['snmp_priv_protocol'],
+                $hostrecord_array['snmp_context'] );
+                break;
+
+	default:	
+		$snmpserialno = ".1.3.6.1.2.1.47.1.1.1.1.11.1001";
+
+		$serialno = cacti_snmp_get( $hostrecord_array['hostname'], $hostrecord_array['snmp_community'], $snmpserialno, 
+		$hostrecord_array['snmp_version'], $hostrecord_array['snmp_username'], $hostrecord_array['snmp_password'], 
+		$hostrecord_array['snmp_auth_protocol'], $hostrecord_array['snmp_priv_passphrase'], $hostrecord_array['snmp_priv_protocol'],
+		$hostrecord_array['snmp_context'] );
+	}
+
+	return $serialno;
+}
+
+function extdb_log( $text ){
+    cacti_log( $text, false, "EXTENDDB" );
 }
 
 ?>
